abf0d62f99226f71c1742709006b551ccdc55478
jardsonbaia <jardsonbaia@hotmail.com>
2021-03-26 22:11:38 -0300

    Modificações no layout

--- models/AlunoSchema.js
+++ models/AlunoSchema.js
@@ -1,5 +1,6 @@
 const  mongoose  = require("mongoose");
 const Schema = mongoose.Schema
+
 const AlunoSchema = new Schema({
     nome: {
         type: String,
--- routers/cursos.js
+++ routers/cursos.js
@@ -5,6 +5,21 @@ router.get('/', (req, res) => {
     res.render("cursos/index")
 })
 
+router.get('/avancado1/', (req, res) => {
+    res.render("cursos/avancado1")
+})
+
+router.get('/avancado2/', (req, res) => {
+    res.render("cursos/avancado2")
+})
+
+router.get('/avancado3/', (req, res) => {
+    res.render("cursos/avancado3")
+})
+
+router.get('/avancadokids/', (req, res) => {
+    res.render("cursos/avancadokids")
+})
 
 
 module.exports = router
